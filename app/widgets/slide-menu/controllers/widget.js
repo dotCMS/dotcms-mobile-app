0d6ef36c48f2b3438b4eb887851b8ae2a03b054f
Freddy Montes <freddymontes@gmail.com>
2014-07-19 16:03:16 -0600

    Add animateLeftPanel option to slide menu widget

--- app/widgets/slide-menu/controllers/widget.js
+++ app/widgets/slide-menu/controllers/widget.js
@@ -1,13 +1,14 @@
 //TODO: add drag-swipe action to the menu;
 
 $.args = arguments[0] || {};
-var slideMenuwidth = $.args.width || 200;
+var animateLeftPanel = $.args.animateLeftPanel || false;
 var duration = $.args.duration || 200;
 var mainContainer;
 var menuRows = [];
+var slideMenuwidth = $.args.width || 200;
 
 $.slideMenu.width = slideMenuwidth;
-$.slideMenu.left = -(slideMenuwidth);
+$.slideMenu.left = animateLeftPanel ? -(slideMenuwidth) : 0;
 
 function createRow(option) {
     var row = Ti.UI.createTableViewRow({
@@ -33,28 +34,30 @@ function populateMenu() {
 
 $.setMainContainer = function(container) {
     mainContainer = container;
+    mainContainer.left = 0;
 }
 
 $.toggleMenu = function () {
-    var slideMenuPosition = $.slideMenu.left < 0 ? 0 : -(slideMenuwidth);
-    var mainContainerPosition = slideMenuPosition == 0 ? slideMenuPosition + slideMenuwidth : 0;
+    if (animateLeftPanel) {
+        var slideMenuPosition = $.slideMenu.left < 0 ? 0 : -(slideMenuwidth);
+        var slideMenuAnimation = Ti.UI.createAnimation({
+            left: slideMenuPosition,
+            duration: duration
+        });
+        $.slideMenu.animate(slideMenuAnimation, function() {
+            $.slideMenu.left = slideMenuPosition;
+        });
+    }
 
-    var slideMenuAnimation = Ti.UI.createAnimation({
-        left: slideMenuPosition,
-        duration: duration
-    });
-
-    $.slideMenu.animate(slideMenuAnimation, function() {
-        $.slideMenu.left = slideMenuPosition;
-    });
-
+    var mainContainerPosition = mainContainer.left == 0 ? mainContainer.left + slideMenuwidth : 0;
     var mainContainerAnimation = Ti.UI.createAnimation({
         left: mainContainerPosition,
         duration: duration
     });
-
     mainContainer.width = Alloy.Globals.screenWidth;
-    mainContainer.animate(mainContainerAnimation);
+    mainContainer.animate(mainContainerAnimation, function() {
+        mainContainer.left = mainContainerPosition;
+    });
 }
 
 populateMenu();
