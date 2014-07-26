1ece057bddc68153a3e1d5984f513497aa476d27
Freddy Montes <freddymontes@gmail.com>
2014-07-25 18:25:35 -0600

    Fix the duplicated toggleSlide event

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,13 @@ Alloy.Globals.openWindow(Alloy.Globals.menuOptions[0]);
 
 // From the slide-menu widget trigger this event to open every menu section
 Ti.App.addEventListener('openSection', function(option) {
+    Ti.App.fireEvent('toggleSlide'); // this one is to close the slide menu
+
     if (option.first) {
         Alloy.Globals.navcontroller.home();
     } else {
         Alloy.Globals.openWindow(option);
     }
-    Ti.App.fireEvent('toggleSlide'); // this one is to close the slide menu
 });
 
 Ti.App.addEventListener('goToHome', function() {
