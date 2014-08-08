7f9829b2b326f580fd89c628452d0ad66d37be7c
Freddy Montes <freddymontes@gmail.com>
2014-08-08 11:24:58 -0600

    Action add to contacts done

--- app/controllers/our-team-detail.js
+++ app/controllers/our-team-detail.js
@@ -1,8 +1,11 @@
 // TODO:
 // Actions:
-// -- Add to contacts
 // -- Share
 // Avatar
+
+// Improve:
+// -- Add to contacts: a cooler way to let the user know the contact
+// has been added and check if the contact exist
 
 $.args = arguments[0] || {};
 var content = $.args.content;
@@ -25,9 +28,62 @@ $.teamMemberEmail.addEventListener('singletap', function() {
 });
 
 $.addToContacts.addEventListener('singletap', function() {
-    console.log('Add to contacts');
+    addNewContact();
 });
 
 $.share.addEventListener('singletap', function() {
     console.log('Share contact');
 });
+
+function addNewContact() {
+    if (Ti.Contacts.contactsAuthorization == Ti.Contacts.AUTHORIZATION_AUTHORIZED){
+        createNewContact();
+    } else if (Ti.Contacts.contactsAuthorization == Ti.Contacts.AUTHORIZATION_UNKNOWN){
+        Ti.Contacts.requestAuthorization(function(e){
+            if (e.success) {
+                createNewContact();
+            } else {
+                addNewContact();
+            }
+        });
+    } else {
+        addNewContact();
+    }
+}
+
+function createNewContact() {
+    var person = {
+        firstName: content.firstName,
+        lastName: content.lastName,
+        organization: 'Dotcms',
+        url: {
+            work: ['www.dotcms.com']
+        },
+        email: {
+            work: []
+        },
+        phone: {
+            work: [],
+            mobile: [],
+            fax: []
+        }
+    };
+
+    if (content.email) {
+        person.email.work = [content.email];
+    }
+    if (content.phone) {
+        person.phone.work = [content.phone.indexOf('ext') != -1 ? content.phone.substr(0, content.phone.indexOf('ext')) : content.phone];
+    }
+    if (content.mobile) {
+        person.phone.mobile = [content.mobile];
+    }
+    if (content.fax) {
+        person.phone.fax = [content.fax];
+    }
+
+    Ti.Contacts.createPerson(person);
+    alert(content.firstName + ' ' + content.lastName + ' contact information has been saved');
+}
+
+
